95270b3587b88a9953ae5ca2c9f7e5d53279820f
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2009-10-22 06:41:44 +0000

    added some debug messages..

--- trunk/src/main2.cpp
+++ trunk/src/main2.cpp
@@ -29,7 +29,7 @@ static const string VERBOSE_3   = "-vvv";
 static const string VERBOSE_4   = "-vvvv";
 static const string VERBOSE_5   = "-vvvvv";
 
-TypedOutputStream tocerr = *StreamFactory::getOutputInstance( cerr );
+//TypedOutputStream tocerr = *StreamFactory::getOutputInstance( cerr );
 
 /*
  * Process the argument list and setup the environment.
@@ -40,7 +40,7 @@ inline bool isOption( const char* str){ return str ? *str == '-' : false; }
 /*
  * Initializes the logging system. 
  */
-void initLog(LogManager& lmgr, const char* out, const int level);
+//void initLog(LogManager& lmgr, const char* out, const int level);
 /*
  * Input and output streams.
  */
@@ -56,36 +56,47 @@ int main(int argc, char** argv, char** env){
   //setup environment
   processArgs( argc, argv, env );
   //Logger has been configured.
-  LogManager lmgr = LogManager::getInstance();
+//  LogManager lmgr = LogManager::getInstance();
   unsigned long int file_no = 0;
   ifstream* in;
   ofstream* out;
   DataRepresentation* data;
-  for (vector< string >::iterator i = input_files.begin(); i != input_files.end(); ++i ){
+  for (vector< string >::iterator i = input_files.begin(); i != input_files.end(); ++i, ++file_no ){
       in = new ifstream( i->c_str() );
       out = new ofstream( (*i + ".cdao").c_str() );
 
       NexusState::setInfile( in );
       NexusState::setOutfile( out );
       
-      lmgr.log(INFO_MESSAGES_LR, "Preparing to parse the input file\n");
+      cerr << "Finished initialization\n";
+      //lmgr.log(INFO_MESSAGES_LR, "Preparing to parse the input file\n");
+ 
+      cerr << "Processing (" << file_no << "/" << input_files.size() << " : " << 100*(double)file_no/input_files.size() << "%):" << *i << endl;
+
       //parse the input stream
       data = nexusparse();
+      cerr << "Parsed: " << *i << endl;
       data->setMatrixLabel( *i  );
-      lmgr.log(INFO_MESSAGES_LR, "Parsed the input file\n");
-      lmgr.log(INFO_MESSAGES_LR, "Preparing to generate output\n");
+      //lmgr.log(INFO_MESSAGES_LR, "Parsed the input file\n");
+      //lmgr.log(INFO_MESSAGES_LR, "Preparing to generate output\n");
       //configure the output generator
       CodeGenerator outputFormatter( data );
+      
+      cerr << "Initialized output formatter\n";
+
       //write the output
       outputFormatter.generate( *out );
-      lmgr.log(INFO_MESSAGES_LR, "Generated output\n");
+
+      cerr << "Generated output\n";
+      //lmgr.log(INFO_MESSAGES_LR, "Generated output\n");
   
       in->close();
       out->close();
       delete in;
       delete out;
       delete data;
-
+      
+      cerr << "Finished Cleanip\n";
   }
 
    
@@ -103,7 +114,7 @@ void processArgs(int argc, char** argv, char** env){
   NexusState::setOutfile( &cout );
   input_files = vector< string >();
   output_files = vector< string >();
-  LogManager lmgr = LogManager::getInstance();
+  //LogManager lmgr = LogManager::getInstance();
   //NexusState::setErrorfile( &cerr );
   for (int i = 1; i < argc  - 1; ++i){
     if ( argv[ i ] == INFILE_ARG){
@@ -118,34 +129,34 @@ void processArgs(int argc, char** argv, char** env){
 	//Codegen::Imports::setBaseURI( argv[ i + 1 ] );
         output_files.push_back( argv[ i + 1 ] );
     }
-    else if ( argv[ i ] == VERBOSE_1){
-      initLog( lmgr, argv[ i + 1], ALERT_MESSAGES_LR ); 
-    }
-    else if ( argv[ i ] == VERBOSE_2){  
-      initLog( lmgr, argv[ i + 1], CRITICAL_MESSAGES_LR ); 
-    }
-    else if ( argv[ i ] == VERBOSE_3){ 
-      initLog( lmgr, argv[ i + 1], ERROR_MESSAGES_LR ); 
-    }
-    else if ( argv[ i ] == VERBOSE_4){
-      initLog( lmgr, argv[ i + 1], INFO_MESSAGES_LR );
-    }
-    else if ( argv[ i ] == VERBOSE_5){ 
-      initLog( lmgr, argv[ i + 1], ALL_MESSAGES_LR );
-    }
+    //else if ( argv[ i ] == VERBOSE_1){
+    //  initLog( lmgr, argv[ i + 1], ALERT_MESSAGES_LR ); 
+    //}
+    //else if ( argv[ i ] == VERBOSE_2){  
+    //  initLog( lmgr, argv[ i + 1], CRITICAL_MESSAGES_LR ); 
+    //}
+    //else if ( argv[ i ] == VERBOSE_3){ 
+    //  initLog( lmgr, argv[ i + 1], ERROR_MESSAGES_LR ); 
+    //}
+    //else if ( argv[ i ] == VERBOSE_4){
+    //  initLog( lmgr, argv[ i + 1], INFO_MESSAGES_LR );
+    //}
+    //else if ( argv[ i ] == VERBOSE_5){ 
+    //  initLog( lmgr, argv[ i + 1], ALL_MESSAGES_LR );
+    //}
   }
  // cerr << "exiting processArgs()\n";
   return;
 }
 
-void initLog(LogManager& lmgr, const char* out, const int level){
+//void initLog(LogManager& lmgr, const char* out, const int level){
 
-  if (isOption( out )){
+//  if (isOption( out )){
     
-    lmgr.registerListener( LoggerFactory::getLogger( tocerr, level  ) );
-  }
-  else {
-    ofstream logStream( out );
-    lmgr.registerListener( LoggerFactory::getLogger( logStream, level  ) );
-  } 
-}
+//    lmgr.registerListener( LoggerFactory::getLogger( tocerr, level  ) );
+ // }
+ // else {
+ //   ofstream logStream( out );
+ //   lmgr.registerListener( LoggerFactory::getLogger( logStream, level  ) );
+ // } 
+//}
