bef97f5810dbc0fc5a538a4be359d363bc66ab85
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2009-10-26 07:16:48 +0000

    Added -z option to cdao-mass-import. This option causes all output files to be saved in gzip format. Fixed cdao url in constants.hpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -3,6 +3,12 @@
  * Brandon Chisham
  * Prototype to read nexus files and translate into the new ontology.
  */
+#include <unistd.h>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #include <string>
 #include <vector>
@@ -23,6 +29,7 @@ using namespace std;
 using namespace CDAO;
 static const string INFILE_ARG  = "-i";
 static const string OUTFILE_ARG = "-o";
+static const string COMPRESS_OUTPUT_ARG = "-z";
 static const string VERBOSE_1   = "-v";
 static const string VERBOSE_2   = "-vv";
 static const string VERBOSE_3   = "-vvv";
@@ -35,6 +42,9 @@ static const string VERBOSE_5   = "-vvvvv";
  * Process the argument list and setup the environment.
  */
 void processArgs(int argc, char** argv, char** env);
+
+void doCompressedOutput( const string& inname, const string& outname );
+void doNormalOutput( const string& inname, const string& outname );
 
 inline bool isOption( const char* str){ return str ? *str == '-' : false; }
 /*
@@ -52,54 +62,23 @@ ostream* out;
 vector< string > input_files;
 vector< string > output_files;
 
+bool compress_output = false;
+
 int main(int argc, char** argv, char** env){
   //setup environment
   processArgs( argc, argv, env );
   //Logger has been configured.
 //  LogManager lmgr = LogManager::getInstance();
-  unsigned long int file_no = 1;
-  ifstream* in;
-  ofstream* out;
-  DataRepresentation* data;
+  unsigned long int file_no = 1;     
   for (vector< string >::iterator i = input_files.begin(); i != input_files.end(); ++i, ++file_no ){
       in = new ifstream( i->c_str() );
-      out = new ofstream( (*i + ".cdao").c_str() );
-
-      NexusState::setInfile( in );
-      NexusState::setOutfile( out );
       
-      //cerr << "Finished initialization\n";
-      //lmgr.log(INFO_MESSAGES_LR, "Preparing to parse the input file\n");
- 
       cerr << "Processing (" << file_no << "/" << input_files.size() << " : " << 100*(double)file_no/input_files.size() << "%):" << *i << endl;
 
-      //parse the input stream
-      data = nexusparse();
-      //cerr << "Parsed: " << *i << endl;
-      data->setMatrixLabel( *i  );
-      //lmgr.log(INFO_MESSAGES_LR, "Parsed the input file\n");
-      //lmgr.log(INFO_MESSAGES_LR, "Preparing to generate output\n");
-      //configure the output generator
-      CodeGenerator outputFormatter( data );
-      
-      //cerr << "Initialized output formatter\n";
-
-      //write the output
-      outputFormatter.generate( *out );
-
-      //cerr << "Generated output\n";
-      //lmgr.log(INFO_MESSAGES_LR, "Generated output\n");
-  
-      in->close();
-      out->close();
-      delete in;
-      delete out;
-      delete data;
-      
-      //cerr << "Finished Cleanip\n";
+      if (compress_output){ doCompressedOutput(*i, *i + ".cdao"); }
+      else { doNormalOutput( *i, *i + ".cdao" ); }
+                         
   }
-
-   
   return 0;
 
 }
@@ -128,6 +107,9 @@ void processArgs(int argc, char** argv, char** env){
       	//NexusState::setOutfile( out =  dynamic_cast<ostream*>(outf) );
 	//Codegen::Imports::setBaseURI( argv[ i + 1 ] );
         output_files.push_back( argv[ i + 1 ] );
+    }
+    else if ( argv[ i ] == COMPRESS_OUTPUT_ARG){
+        compress_output = true;
     }
     //else if ( argv[ i ] == VERBOSE_1){
     //  initLog( lmgr, argv[ i + 1], ALERT_MESSAGES_LR ); 
@@ -160,3 +142,115 @@ void processArgs(int argc, char** argv, char** env){
  //   lmgr.registerListener( LoggerFactory::getLogger( logStream, level  ) );
  // } 
 //}
+
+void doCompressedOutput( const string& inname, const string& outname ){
+  ifstream* in;
+  //ofstream* out;
+  DataRepresentation* data;
+  const unsigned int READ_END = 0;
+  const unsigned int WRITE_END = 1;
+  int pipe_descriptors[2];
+  if (pipe(pipe_descriptors) > -1){
+      pid_t child_pid = fork();
+      if (0 == child_pid){
+        /* child-code */
+        if ( close(pipe_descriptors[ WRITE_END ]) > -1 ){
+          if (dup2( pipe_descriptors[ READ_END ], STDIN_FILENO ) > -1){
+            int outfiledes = open( (outname + ".gz").c_str(), O_CREAT|O_WRONLY|O_TRUNC,0644);
+            if (outfiledes > -1){
+              if (dup2( outfiledes, STDOUT_FILENO ) > -1){
+                  close( outfiledes );
+                  execlp("gzip","gzip", (char*)NULL);
+              }
+              else {
+                perror("Unable to dup STDOUT_FILENO ");
+                exit( 1 );
+              }
+            }
+            else {
+              perror(("unable to open " + outname + ".gz ").c_str());
+              exit( 1 );
+            }
+          }
+          else {
+             perror("Unable to dup STDIN_FILENO ");
+             exit( 1 );
+          }
+
+        }
+        else {
+          perror("Unable to close write end of pipe");
+          exit( 1 );
+        }
+      }
+      else {
+        if (close( pipe_descriptors[ READ_END ]) > -1 ){
+          if (dup2( pipe_descriptors[ WRITE_END ], STDOUT_FILENO) > -1){
+            in = new ifstream( inname.c_str() );
+            NexusState::setInfile( in );
+            NexusState::setOutfile( &cout );
+
+            data = nexusparse();
+
+            cerr << "Parsed: " << inname << endl;
+
+            data->setMatrixLabel( inname );
+            CodeGenerator outputFormatter( data );
+
+            outputFormatter.generate( cout );
+        
+            cerr << "Wrote output" << endl;
+
+            in->close();
+            close( pipe_descriptors[ WRITE_END ] );
+            close( STDOUT_FILENO );
+            delete in;
+            delete data;
+            int status; 
+            cerr << "Waiting for child to exit" << endl;
+            waitpid(-1, &status, 0);
+            cerr << "Child Exited" << endl;
+          }
+          else { perror( "Unable to dup STDOUT_FILENO" ); }
+        }
+        else { perror( "Unable to close read end of pipe" ); }
+      }
+  }
+  else {
+      perror("Unable to open pipe: ");
+      exit( 1 );
+
+  }
+ // delete data;
+
+}
+
+void doNormalOutput( const string& inname, const string& outname ){
+
+  ifstream* in;
+  ofstream* out;
+  DataRepresentation* data;
+
+  in = new ifstream( inname.c_str() );
+  out = new ofstream( outname.c_str() );
+
+  NexusState::setInfile( in );
+  NexusState::setOutfile( out );
+  
+  data = nexusparse();
+
+  data->setMatrixLabel( inname );
+
+  CodeGenerator outputFormatter( data );
+
+  outputFormatter.generate( *out );
+
+  in->close();
+  out->close();
+  delete in;
+  delete out;
+  delete data;
+
+
+}
+
