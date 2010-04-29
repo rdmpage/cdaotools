05daec65025058a9d15d38b7aba7cba9863762a1
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2010-04-29 01:27:31 +0000

    Made php versions of the homepage.

---
+++ src/public_html/BlackWhiteSimple/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">  <hr/>  
+	WARNING: This is still in alpha-testing.  There are known bugs that will crash the application.  If you are using this and a tree crashes or does not display properly, please email <a href="mailto:cdaotools@gmail.com">us</a>
+    
+     <p about=""
+     resource="http://www.w3.org/TR/rdfa-syntax"
+          rel="dct:conformsTo" xmlns:dc="http://purl.org/dc/terms/">
+	      <a href="http://validator.w3.org/check?uri=referer" ><img
+	              src="http://www.w3.org/Icons/valid-xhtml-rdfa-blue"
+		              alt="Valid XHTML + RDFa" style="border:0px;"   /></a>
+			        </p>
+
+  </div>
---
+++ src/public_html/BlackWhiteSimple/header.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+      xmlns:cc="http://creativecommons.org/ns#"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:foaf="http://xmlns.com/foaf/0.1/"
+      xmlns:cdao="http://www.evolutionaryontology.org/cdao.owl#"
+      xmlns:study="http://www.cs.nmsu.edu/~bchisham/study.owl#"
+      xml:lang="en">
+ <head profile="http://www.w3.org/1999/xhtml/vocab">
+      <title>Cdao Store Query System: Home</title>
+      <link rel="stylesheet" type="text/css" href="style.css"/>
+      <link rel="alternate" title="Project News" href="http://sourceforge.net/export/rss2_keepsake.php?group_id=282379" type="application/rss+xml"/>
+      <link rel="SHORTCUT ICON" href="cdao-store-favicon.ico" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+ <body resource="index.html">
+   <div id="wrap"><div id="header">
+    <p><h1 property="dc:title">
+    <a href="index.html"><img src="cdao-triplestore-logo.jpg" alt="Cdao store logo" style="border:0px;" /></a>CDAO Store</h1></p></div>
+    <div id="menu">
+      <a href="index.html">Home</a> | 
+      <a href="faq.html">FAQ</a> |
+      <a href="tree-chooser.jnlp">Browser</a> |
+      <a href="phylows.html">PhyloWS</a> |
+      <a href="http://cdaotools.sourceforge.net">Download</a>
+    </div>
