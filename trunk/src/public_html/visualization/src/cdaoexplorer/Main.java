1dfcda2eedbab4e0d5732f6ea832089e29d9fd58
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-04-29 05:23:20 +0000

    Created a small main application that ties together the matrix and tree viewers.

---
+++ trunk/src/public_html/visualization/src/cdaoexplorer/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cdaoexplorer;
+
+import cdaoexplorer.forms.Workbench;
+
+/**
+ *
+ * @author bchisham
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //JFrame workbench = new Workbench();
+       Workbench.main(args);
+
+    }
+
+}
