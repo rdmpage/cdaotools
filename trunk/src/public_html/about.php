c9c94dfc1f86e8c822a17a92717810ab9034b060
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-05-01 02:20:27 +0000

    Converted basic pages to php Added style for list items to do indentation

--- trunk/src/public_html/about.php
+++ trunk/src/public_html/about.php
@@ -1,25 +1,33 @@
 <?php require_once 'header.php' ?>
 <div id="content">
 
-<h1>Frequently Asked Questions</h1>
-<ul>
-  <li>What is CDAO</li>
-  <li>What is PhlyoWS?</li>
-  <li>Can I inject SPARQL queries?</li>
-  <li>How do I import a new data file?</li>
-  <li>How do I navigate the graphical browser?</li>
-  <li>Where do the tree names come from?</li>
-  <li>How do I get raw RDF/XML files of the tree/matrix?</li>
-  <li>Can I export to a non-CDAO filetype?</li>
-  <li>It appears the browser is not displaying a tree correctly</li>
-</ul>
+<h1>About Us</h1>
+<p>We are located in the Department of <a href="www.cs.nmsu.edu">Computer Science</a> at <a href="www.nmsu.edu">New Mexico State University</a>.</p>
 
-<br/>
-<hr/>
-<br/>
+<h3>People:</h3>
+<ul>
+	<li> Professors:
+		<ul>
+			<li>Enrico Pontelli</li>
+			<li>Son Cao Tran</li>
+		</ul>
+	</li>
+	<li> Graduate Students:
+		<ul>
+			<li>Brandon Chisham</li>
+			<li>Trung Le</li>
+			<li>Ben Wright</li>
+		</ul>
+	</li>
+</ul>
+<h3>Publications:</h3>
+<ul>
+	<li><span style="font-weight: bold;">CDAO-Store: A New Vision for Data Integration</span>: to be presented at <a href="http://ievobio.org">iEvoBio2010</a></li>
+</ul>
 <p>
-Other questions? Email:<a href="mailto:cdaotools@gmail.com">us</a>
-</p>
+This project is currently funded by NSF CREST grant HRD-0420407 and NSF IGERT grant DGE-0504304.</p>
+
+
 </div><div id="footer"><hr/>   
 WARNING: This is still in alpha-testing.  There are known bugs that will crash the application.  If you are using this and a tree crashes or does not display properly, please email <a href="mailto:cdaotools@gmail.com">us</a>
     
