1c71c189533bb58d8ca081cee4cde265c0e35fe2
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-05-04 20:09:13 +0000

    Created a style for result sets. Updated tu, study, algorithm, author, and program queries to use the new stylesheet. Algorithm and program queries now desplay the thing queried for when showing the results. Added a result count to the program results.

--- trunk/src/public_html/bin/do_html_program_query.sh
+++ trunk/src/public_html/bin/do_html_program_query.sh
@@ -3,11 +3,11 @@
 # File:   do_html_study_query.sh
 # Author: bchisham
 #
-# Created on March 4, 2010, 12:20 AM
+# Created on March 4, 2010, 12:50 PM
 #
 
 #Setup the environment
-export ALGORITHM=`echo $QUERY_STRING |grep -oE "(^|[?&])algorithm=[^&]+" | sed "s/%20/ /g" | sed "s/+/ /g"  | cut -f 2 -d "="`
+export SOFTWARE=`echo $QUERY_STRING |grep -oE "(^|[?&])software=[^&]+" | sed "s/%20/ /g" | sed "s/+/ /g" | cut -f 2 -d "="`
 export GRAPH_FILE="../../dump_utf8.txt.owl"
 export ESCAPED_DUMP_URI="http:\/\/www.cs.nmsu.edu\/~bchisham\/dump_utf8.txt.owl#"
 #export STUDY_RESULT_TRANSFORM="../../cdao-store/study_result_to_html.xsl"
@@ -15,9 +15,29 @@ export ESCAPED_DUMP_URI="http:\/\/www.cs.nmsu.edu\/~bchisham\/dump_utf8.txt.owl#
 export TREE_URI="../tu/html?last="
 source ~/.bashrc
 
-export ALGORITHM_QUERY=$( do_algorithm_list_query_construct.sh "$ALGORITHM" )
+#export ALGORITHM_QUERY=$( do_algorithm_list_query_construct.sh "$ALGORITHM" )
 #export AUTHOR_QUERY=$( do_study_list_authors.sh "$STUDY" )
 #export TREE_QUERY=$( do_study_list_trees.sh "$STUDY" )
+
+
+function print_query {
+ALGO="$1"
+cat << EOM
+PREFIX study: <http://www.cs.nmsu.edu/~bchisham/study.owl#>
+PREFIX contact: <http://www.w3.org/2000/10/swap/pim/contact#>
+PREFIX foaf: <http://www.mindswap.org/2003/owl/foaf#>
+SELECT ?tree 
+WHERE {
+    ?study study:has_analysis ?analysis .
+    ?analysis study:has_software '$ALGO' .
+    ?analysis study:has_output_tree ?tree .
+}
+  
+
+EOM
+}
+
+export SOFTWARE_QUERY=`print_query $SOFTWARE`;
 
 #Print the HTML header information
 function print_header {
@@ -43,11 +63,11 @@ Content-type: text/html; charset: utf-8
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
  </head>
  <body about="http://www.evolutionaryontology.org/cdao.owl">
-  <div class="wrap">
-   <div class="header">
+   <div class="wrap">
+     <div class="header">
        <h1 class="header"><a href="../../cdao-store/index.html"><img src="../../cdao-triplestore-logo.jpg" alt="Cdao-Store Logo" style="border: 0px;" /></a>Cdao Store Query System</h1>
-   </div>
-   <div class="content" style="scroll: auto;">
+     </div>
+   <div class="main-content" style="scroll: auto;">
 EOM
 }
 
@@ -60,18 +80,18 @@ cat << EOM
 </html>
 EOM
 }
-#Print the user form if the $STUDY variable is unset.
+#Print the user form if the $SOFTWARE variable is unset.
 function print_form {
 cat << EOM
    <table><form action="html" metod="get">
-     <tr><td>Algorithm:</td><td><input type="text" class="text" id="algorithm" name="algorithm" value="$ALGORITHM"></td></tr>
+     <tr><td>Algorithm:</td><td><input type="text" class="text" id="software" name="software" value="$SOFTWARE"></td></tr>
      <tr><td colspan="2"><input class="button"type="submit" value="Submit"/><input class="button" type="reset" value="Reset"/><td></tr>
    </form></table>
 EOM
 }
 
 function print_result {
-   echo "<!-- ALGORITHM_QUERY: $ALGORITHM_QUERY -->"
+   echo "<!-- SOFTWARE_QUERY: $SOFTWARE_QUERY -->"
    #echo "<!-- AUTHOR_QUERY: $AUTHOR_QUERY -->"
    #echo "<!-- TREE_QUERY: $TREE_QUERY -->"
    #echo "<h2>Listing detail for study id: $STUDY</h2>"
@@ -79,7 +99,7 @@ function print_result {
    #TMP_RESULTS_FILE=`mktemp`".xml";
    #TMP_AUTHOR_QUERY_FILE=`mktemp`".rql";
    #TMP_TREE_QUERY_FILE=`mktemp`".rql"
-   echo "$ALGORITHM_QUERY" > $TMP_QUERY_FILE;
+   echo "$SOFTWARE_QUERY" > $TMP_QUERY_FILE;
   # echo "$AUTHOR_QUERY" > $TMP_AUTHOR_QUERY_FILE;
   # echo "$TREE_QUERY" > $TMP_TREE_QUERY_FILE;
    #echo "<pre>";
@@ -87,24 +107,24 @@ function print_result {
    #sparql -q --results xml --query "$TMP_AUTHOR_QUERY_FILE" --graph "$GRAPH_FILE" | xsltproc $STUDY_AUTHORS_TRANSFORM -
    #echo "<h3>Citation and Abstract</h3>"
    #sparql -q --results xml --query "$TMP_QUERY_FILE" --graph "$GRAPH_FILE" | xsltproc $STUDY_RESULT_TRANSFORM - #| perl -p -n -e "s/^ *\n//g" | head -n 1
-   RESULT_SIZE=0;
-   echo "<h3>Trees Constructed with the \"$ALGORITHM\" Algorithm</h3>"
+   echo "<h3>Trees Constructed with the $SOFTWARE Program</h3>"
    echo "<div class=\"result-set\">"
-   for result in $(sparql -q --results xml --query "$TMP_QUERY_FILE" --graph "$GRAPH_FILE" | grep -oE "<uri>.*</uri>" | sort | cut -d">" --fields=2 | cut -d"<" --fields=1 | sed "s/$ESCAPED_DUMP_URI//g");
+   ROW_COUNT=0;
+   for result in $(sparql -q --results xml --query "$TMP_QUERY_FILE" --graph "$GRAPH_FILE" | grep -oE "<uri>.*</uri>" | cut -d">" --fields=2 | cut -d"<" --fields=1 | sed "s/$ESCAPED_DUMP_URI//g" | sort);
    do
-        RESULT_SIZE=$[ $RESULT_SIZE + 1 ];
-        echo "<a href=\"../tree/html?tree=$result\">$result</a><br/>";
+        echo "<a href=\"../tree/query?format=html&amp;tree=$result\">$result</a><br/>";
+        ROW_COUNT=$[ $ROW_COUNT + 1 ];
    done
-   echo "</div>"
-   echo "<p>Results: $RESULT_SIZE</p>"
+   echo "<span class=\"result-count\">$ROW_COUNT</span>"
    #echo "</pre>";
+   echo "</div>"
    rm -f "$TMP_QUERY_FILE" 
 }
 
 
 print_header;
 
-if [[ $ALGORITHM == "" ]]; then
+if [[ $SOFTWARE == "" ]]; then
    print_form;
 else
    print_result;
