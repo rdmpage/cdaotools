cea2a7f92e84fff3390a299ea7ed00c7431fc0ce
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2010-05-27 01:11:13 +0000

    Added nexus format writer.

---
+++ src/codegen/nexus/nexus-codegen.hpp
@@ -0,0 +1,30 @@
+#ifndef NEXUS_CODEGEN_HPP_
+#define NEXUS_CODEGEN_HPP_
+
+#include <fstream>
+#include <cassert>
+#include <data_representation.hpp>
+#include <codegen/codegen.hpp>
+
+namespace CDAO {
+
+  class NexusCodeGenerator : public CodeGenerator {
+	public:
+	   /**
+	    * Create a generator for the specified model.
+	    */
+	   NexusCodeGenerator( const DataRepresentation* data_model ):CodeGenerator(data_model){}
+	   /**
+	    * Cleanup
+	    */
+	   virtual ~NexusCodeGenerator();
+	   /**
+	    * Write a nexus format version of the data-model to the specified stream.
+	    */
+	   virtual void generate( std::wostream& out );
+
+  };
+
+}
+
+#endif
