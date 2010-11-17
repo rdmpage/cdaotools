0befaa819e6e13d1e917c09b821588693dd860c1
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2010-11-17 02:13:24 +0000

    updated scripts.

--- src/test/bin/do_jobs.sh
+++ src/test/bin/do_jobs.sh
@@ -4,6 +4,6 @@ JOBS_FILE="$1"
 JOB_NO=0;
 
 for i in $(cat $JOBS_FILE); do
-	time (curl "$i" >/dev/null) > `hostname`."$JOB_NO";
+	time (curl "$i" >/dev/null) 2>> `hostname`;
 	JOB_NO=$[$JOB_NO + 1];
 done
