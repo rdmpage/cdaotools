b7e54daaa049b2aee6d5838737ffaf72bb1c10e5
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-11-17 02:13:24 +0000

    updated scripts.

--- trunk/src/test/bin/run_tests.sh
+++ trunk/src/test/bin/run_tests.sh
@@ -3,10 +3,9 @@
 export HOSTS_FILE="$1"
 export JOBS_FILE="$2"
 
-scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
-ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
+#scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
+#ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
 
 for host in $(cat $HOSTS_FILE); do
-	scp "$JOBS_FILE" "$host":"~/$JOBS_FILE"
 	ssh "$host" "do_jobs.sh '$JOBS_FILE'"
 done
