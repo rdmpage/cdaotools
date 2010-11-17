6e55f377733bb25459dd56251c42b70eef198b08
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-11-17 01:26:36 +0000

    jobs framework ready

--- trunk/src/test/bin/do_jobs.sh
+++ trunk/src/test/bin/do_jobs.sh
@@ -4,6 +4,6 @@ JOBS_FILE="$1"
 JOB_NO=0;
 
 for i in $(cat $JOBS_FILE); do
-	time ("$i" >/dev/null) > `hostname`."$JOB_NO";
+	time (curl "$i" >/dev/null) > `hostname`."$JOB_NO";
 	JOB_NO=$[$JOB_NO + 1];
 done
