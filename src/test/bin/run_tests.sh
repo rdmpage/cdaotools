46dea77bc2ea3c4c318e0598a6d4dbfee2948b01
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2010-11-17 01:23:58 +0000

    Changed the way to run jobs.

--- src/test/bin/run_tests.sh
+++ src/test/bin/run_tests.sh
@@ -3,9 +3,10 @@
 export HOSTS_FILE="$1"
 export JOBS_FILE="$2"
 
+scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
+ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
 
 for host in $(cat $HOSTS_FILE); do
-	for job in $(cat $JOBS_FILE); do
-		(ssh "$host" && time ($job >/dev/null ) > "$host.$job.time");
-	done
+	scp "$host":"~/$JOBS_FILE"
+	ssh "$host" "do_jobs.sh '$JOBS_FILE'"
 done
