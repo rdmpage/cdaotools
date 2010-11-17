cf59880f2c4e3ca714165902030cc9d2cb8835cc
Brandon Chisham <7194010+bchisham@users.noreply.github.com>
2010-11-17 01:26:36 +0000

    jobs framework ready

--- src/test/bin/run_tests.sh
+++ src/test/bin/run_tests.sh
@@ -7,6 +7,6 @@ scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
 ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
 
 for host in $(cat $HOSTS_FILE); do
-	scp "$host":"~/$JOBS_FILE"
+	scp "$JOBS_FILE" "$host":"~/$JOBS_FILE"
 	ssh "$host" "do_jobs.sh '$JOBS_FILE'"
 done
