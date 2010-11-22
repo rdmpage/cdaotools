edf3c5d339f544a9e01cd8db138128f745657d49
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-11-22 04:29:33 +0000

    Restructured phylows tree query. Should address the bugs the export bug.

--- trunk/src/public_html/bin/do_phylows_unordered_tree.sh
+++ trunk/src/public_html/bin/do_phylows_unordered_tree.sh
@@ -60,7 +60,7 @@ EOM
       echo "<rdf:RDF xmlns=\"http://www.evolutionaryontology.org/cdao.owl#\"
         xml:base=\"http://www.evolutionaryontology.org/cdao.owl\"
         xmlns:owl2xml=\"http://www.w3.org/2006/12/owl2-xml#\"
-        xmlns:cdao=\"http://www.evolutionaryontology.org/cdao.owl#\"
+        xmlns:cdao=\"http://www.evolutionaryontology.org/cdao/1.0/cdao.owl#\"
         xmlns:xsd=\"http://www.w3.org/2001/XMLSchema#\"
         xmlns:dc=\"http://purl.org/dc/elements/1.1/\"
         xmlns:owl2=\"http://www.w3.org/2006/12/owl2#\"
@@ -70,9 +70,19 @@ EOM
 
       echo -e "<!-- NODE: $NODE_QUERY \n EDGE: $EDGE_QUERY \n REQUEST_URI: $REQUEST_URI \n  -->"
       echo "<owl:Ontology rdf:about=\"\"></owl:Ontology>"
-      echo "<cdao:Tree rdf:resource=\"$TREE_URI\"/>"
-      do_query.py "$TRIPLESTORE_CONFIG_STRING" "$TREE_URI"  "$NODE_QUERY" "<cdao:Node rdf:resource=\"%s\"><cdao:part_of rdf:resource=\"$TREE_URI\"/><!-- %s --></cdao:Node>" "$EDGE_QUERY" "<cdao:DirectedEdge rdf:resource=\"%s\"><cdao:part_of rdf:resource=\"$TREE_URI\"/>  <cdao:has_Parent_Node rdf:resource=\"%s\"/> <cdao:has_Child_Node rdf:resource=\"%s\"/></cdao:DirectedEdge>" 
-   #echo "</cdao:Tree>"
+      echo "<cdao:Tree rdf:about=\"$TREE_URI\"/>"
+      echo "  <!-- <rdf:type rdf:about=\"$CDAONS""Tree\" /> -->"
+      do_query.py "$TRIPLESTORE_CONFIG_STRING" "$TREE_URI"  "$NODE_QUERY" "
+      <cdao:Node rdf:about=\"%s\">
+        <cdao:belongs_to_Tree rdf:resource=\"$TREE_URI\"/>
+      </cdao:Node>
+         <!-- %s -->
+      " "$EDGE_QUERY" "
+      <cdao:DirectedEdge rdf:about=\"%s\">
+         <cdao:belongs_to_Tree rdf:resource=\"$TREE_URI\"/>
+         <cdao:has_Parent_Node rdf:resource=\"%s\"/>
+         <cdao:has_Child_Node rdf:resource=\"%s\"/>
+      </cdao:DirectedEdge>" 
      echo "</rdf:RDF>"
   fi
 fi
