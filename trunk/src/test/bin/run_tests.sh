f74f34e0abd5efa40d2d9d7276d4f43e95f28ced
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-11-29 20:59:33 +0000

    updated run_tests.sh Created list of phylows jobs

--- trunk/src/test/bin/run_tests.sh
+++ trunk/src/test/bin/run_tests.sh
@@ -6,6 +6,10 @@ export JOBS_FILE="$2"
 #scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
 #ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
 
-for host in $(cat $HOSTS_FILE); do
-	ssh "$host" "do_jobs.sh '$JOBS_FILE'"
+for job in $(cat $JOBS_FILE); do
+	echo "Starting on job: $job"
+	for host in $(cat $HOSTS_FILE); do
+		echo "Runing on host $host"
+		ssh "$host" "do_job.sh '$job'"
+	done
 done
