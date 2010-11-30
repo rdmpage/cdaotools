f4aadea6166b6470cc4f0cb27261a62813b31082
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-11-30 18:43:08 +0000

    Updated local run test scripts.

--- trunk/src/test/bin/run_local_tests.sh
+++ trunk/src/test/bin/run_local_tests.sh
@@ -5,11 +5,12 @@ export JOBS_FILE="$2"
 
 #scp do_jobs.sh daedalus.cs.nmsu.edu:~/bin/
 #ssh daedalus.cs.nmsu.edu "chmod 700 ~/bin/do_jobs.sh"
+exec 3< $JOBS_FILE
 
-for job in $(cat $JOBS_FILE); do
-	echo "Starting on job: $job"
+while read -r -a job <&3; do
+	echo "Starting on job: ${job[1]}"
 	for host in $(cat $HOSTS_FILE); do
 		echo "Runing on host $host"
-		ssh "$host" "do_job.sh '$job'"
+	        time ssh "$host" "do_local_job.sh '${job[0]}' '${job[1]}'"
 	done
 done
