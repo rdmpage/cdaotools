be96380ad3b3eeb1477dc50554677deb4a6ac315
bchisham <bchisham@79bd73f5-f106-48ad-9d9b-fde1f9e9edec>
2010-12-11 01:11:54 +0000

    Broke up unordered tree query into format specific scripts.

--- trunk/src/public_html/bin/do_phylows_tree.sh
+++ trunk/src/public_html/bin/do_phylows_tree.sh
@@ -4,10 +4,10 @@
 #May 8, 2010
 #Script to produce ordered query results for the phylows service.
 source ~/.bashrc
-export FORMAT=$(echo "$QUERY_STRING" | grep -oE "format=[a-zA-Z][a-zA-Z]*" | cut -d= --fields=2)
+export FORMAT=$(echo "$QUERY_STRING" | grep -oE "format=(graphml|prolog|nexml|nexus|rdf|cdao)" | cut -d= --fields=2)
 
 if [[ "$FORMAT" == "graphml" ]]; then
    do_phylows_unordered_tree.sh | order_graphml.sh
 else
-   do_phylows_unordered_tree.sh
+   do_"$FORMAT"_tree.sh
 fi
